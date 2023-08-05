05630c58c129fa4484c34c607243acf7dfabaf1b
Christopher <cagm96@gmail.com>
2023-08-05 18:08:36 -0400

    update

--- scripts/index.js
+++ scripts/index.js
@@ -37,7 +37,7 @@ const profileDescriptionInput = document.querySelector(
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
 
-//cards
+//cards add modal
 const cardListEl = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
@@ -45,26 +45,29 @@ const addNewCardButton = document.querySelector(".profile__add-button");
 const cardAddModal = document.querySelector("#add-card-modal");
 const cardCloseButton = document.querySelector("#cardModalCLose");
 
+//cards
+
 //----------------functions
 
 function closePopop() {
   profileEditModal.classList.remove("modal_open");
   cardAddModal.classList.remove("modal_open");
 }
-function getCardElement(cardData) {
+function getCardElement(data) {
   //clone the template element with all its content and store it in a cardElement variable
   const cardElement = cardTemplate.cloneNode(true);
 
   //  access the card title and image and store them in variables
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
-
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
   // set the path to the image to the link field of the object
-  cardImageEl.src = cardData.link;
+  cardImageEl.src = data.link;
   // set the image alt text to the name field of the object
-  cardImageEl.alt = cardData.name;
+  cardImageEl.alt = data.name;
   // set the card title to the name field of the object, too
-  cardTitleEl.textContent = cardData.name;
+  cardTitleEl.textContent = data.name;
 
   //return the ready HTML element with the filled-in data
   return cardElement;
@@ -97,7 +100,15 @@ addNewCardButton.addEventListener("click", () => {
 
 cardCloseButton.addEventListener("click", closePopop);
 
-initialCards.forEach((cardData) => {
-  const cardElement = getCardElement(cardData);
+likeButton.addEventListener("click", () => {
+  likeButton.classList.toggle("card__like-button_active");
+});
+
+deleteButton.addEventListener("click", () => {
+  cardElement.remove();
+});
+
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
   cardListEl.append(cardElement);
 });
