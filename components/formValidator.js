7e1db844c589bbc8230b259bfbf38e7a7b2fe35b
Christopher <cagm96@gmail.com>
2023-10-13 23:02:41 -0400

    update

--- components/formValidator.js
+++ components/formValidator.js
@@ -1,3 +1,108 @@
 export default class FormValidator {
-constructor()
+  constructor({ selector, formClass }, formEl) {
+    this._selector = selector;
+    this._formClass = formClass;
+    this._formEl = formEl;
+  }
+  //PRIVATE checking the fields validity
+  _hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+    const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+    inputEl.classList.remove(inputErrorClass);
+    errorMessageEl.textContent = "";
+    errorMessageEl.classList.remove(errorClass);
+  }
+
+  //PRIVATE changing the state of the Submit button
+  _toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
+    if (hasInvalidInput(inputEls)) {
+      submitButton.classList.add(inactiveButtonClass);
+      submitButton.disabled = true;
+      return;
+    }
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
+  }
+
+  //PRIVATE Adding all the needed handlers.
+
+  //PUBLIC method enableValidation(), which enables form validation.
+
+  //public method to either disable the state of the button or reset form validation (including the state of the submit button).
+  //    1. the submission button for all forms should be disabled whenever one or more form fields are invalid.
+  //    2. after successfully submitting the card form, the form fields should be cleared and the submit button should be disabled.
+  //       this should only be done after successful submission.
+
+  //       NOTE
+  //       When the form is closed, but not submitted, it provides a better user experience to leave the form fields as they are, so as to prevent lost data.
+  //       Note that for the edit profile modal, the button must be disabled after submission. When the user opens the modal again, it will already be filled with valid input values.
 }
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
+}
+
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(errorClass);
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+  if (!inputEl.validity.valid) {
+    return showInputError(formEl, inputEl, options);
+  }
+  if (inputEl.validity.valid) {
+    return hideInputError(formEl, inputEl, options);
+  }
+}
+
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputEl) => inputEl.validity.valid);
+}
+
+//enableButton
+
+function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
+  if (hasInvalidInput(inputEls)) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+    return;
+  }
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+}
+
+function setEventListeners(formEl, options) {
+  const { inputSelector, submitButtonSelector } = options; // Destructure submitButtonSelector
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  const submitButton = formEl.querySelector(submitButtonSelector); // Use submitButtonSelector
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      checkInputValidity(formEl, inputEl, options);
+      toggleButtonState(inputEls, submitButton, options);
+    });
+  });
+}
+
+function enableValidation(config) {
+  const formEls = [...document.querySelectorAll(config.formSelector)];
+
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+    setEventListeners(formEl, config);
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
