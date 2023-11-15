a08ceaa583a7e1451debfa60693c22a8ae36807d
cagm96 <cagm96@gmail.com>
2023-11-15 12:42:38 -0500

    webpack update'

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import FormValidator from "../components/FormValidator.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
+import "../pages/index.css";
 
 const initialCards = [
   {
